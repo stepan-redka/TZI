10d71af865d9b6a67d8cea02f73d27d44dd6160c
unknown <stepanredka17@gmail.com>
2024-11-07 19:14:07 +0200

    14

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit'); // Імпортуємо express-rate-limit
 
 const app = express();
 const PORT = process.env.PORT || 10000;
@@ -23,29 +24,38 @@ const FormData = mongoose.model('FormData', formDataSchema);
 
 // Налаштування для отримання POST-запитів у форматі JSON
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());  // Додаємо підтримку JSON
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+// Налаштування ліміту запитів
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 хвилин
+    max: 100, // Максимальна кількість запитів за 15 хвилин
+    message: 'Ліміт запитів вичерпано. Спробуйте знову через 15 хвилин.'
+});
+
+// Застосовуємо лімітатор до всіх запитів POST на маршрут /submit
+app.use('/submit', limiter);
 
 // Маршрут для обробки даних з форми
 app.post('/submit', async (req, res) => {
     const { name } = req.body;
-    console.log('Received data:', req.body);  // Логування отриманих даних
+    console.log('Received data:', req.body);
 
     if (!name) {
-        return res.status(400).send('Name is required');  // Якщо немає name, вивести помилку
+        return res.status(400).send('Name is required');
     }
 
     try {
         const formData = new FormData({ name });
-        console.log('Saving data:', formData);  // Логування даних перед збереженням
+        console.log('Saving data:', formData);
 
-        // Спроба зберегти дані
         await formData.save();
 
-        console.log('Data saved');  // Лог після успішного збереження
+        console.log('Data saved');
         res.send('Дані збережено!');
     } catch (error) {
-        console.error('Error saving data:', error);  // Лог на помилку
+        console.error('Error saving data:', error);
         res.status(500).send('Помилка при збереженні даних.');
     }
 });
